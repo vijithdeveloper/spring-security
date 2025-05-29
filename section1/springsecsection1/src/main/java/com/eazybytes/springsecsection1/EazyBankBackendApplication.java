b5db51f01d8ed9fbe925d9c18ea35725358ec698
vijithdeveloper <vijithdeveloper108@gmail.com>
2025-05-29 20:33:22 +0530

    Update EazyBankBackendApplication.java

--- section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
+++ section1/springsecsection1/src/main/java/com/eazybytes/springsecsection1/EazyBankBackendApplication.java
@@ -13,3 +13,36 @@ public class EazyBankBackendApplication {
 	}
 
 }
+
+####################################################
+
+@SpringBootApplication is a convenience annotation that includes:
+
+@Configuration
+
+@EnableAutoConfiguration
+
+@ComponentScan (default is current package and sub-packages)
+
+Adding @ComponentScan(basePackages = {...}) overrides the default scanning behavior, allowing you to include additional packages explicitly.
+
+example
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo", "com.example.services"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+src/main/java
+└── com/example
+    ├── demo
+    │   └── Application.java
+    └── services
+        └── MyService.java
+
+
+	
